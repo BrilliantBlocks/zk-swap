8c249a686748633deee4dfed7b99cc3a2f4caf77
Thomas S. Bauer <tsbauer@posteo.eu>
2023-02-14 09:46:13 +0100

    Refactor title and tagline

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'zk-Swap',
+  tagline: 'NFT AMM on StarkNet',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
