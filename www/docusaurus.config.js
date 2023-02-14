ae283b1c01bf07507eea680915fd3fb287701cbc
Thomas S. Bauer <tsbauer@posteo.eu>
2023-02-14 09:51:19 +0100

    Remove GitHub pages config

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -13,13 +13,7 @@ const config = {
   // Set the production url of your site here
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
